45369a2876659f34617d023204036d0b87681bb6
gene9831 <pacify.98@gmail.com>
2025-01-22 17:34:50 +0800

    feat: card extension

--- src/routes/movies.lazy.tsx
+++ src/routes/movies.lazy.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Box, Card, Grid, Image } from '@chakra-ui/react'
+import { AspectRatio, Box, Card, Center, Grid } from '@chakra-ui/react'
 import { createLazyFileRoute } from '@tanstack/react-router'
 import { useState } from 'react'
 
@@ -24,14 +24,92 @@ const templatePosts = [
 const posts = Array(5).fill(templatePosts).flat()
 
 function Movies() {
-  const [hoverd, setHoverd] = useState('')
+  const initialValueOfHoverd = { id: '', index: -1, row: -1 }
+  const [hoverd, setHoverd] = useState(initialValueOfHoverd)
+  // hoverd的卡片延伸方向
+  const [extensionDirection, setExtensionDirection] = useState<
+    'left' | 'right'
+  >('right')
+  const [columns, setColumns] = useState(0)
 
-  const calcLeft = (id: string) => {
-    if (!hoverd || hoverd === id) {
-      return '0'
+  const calcColumns = (containerElem: HTMLElement, cellElem: HTMLElement) => {
+    const containerRect = containerElem.getBoundingClientRect()
+    const cellRect = cellElem.getBoundingClientRect()
+
+    const spacing = getComputedStyle(cellElem).getPropertyValue(
+      '--container-spacing',
+    )
+    const fontSize = getComputedStyle(document.documentElement).fontSize
+
+    return Math.ceil(
+      containerRect.width /
+        (cellRect.width + parseFloat(spacing) * parseFloat(fontSize)),
+    )
+  }
+
+  const handleMouseEnter = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    id: string,
+    index: number,
+  ) => {
+    console.log(index)
+    const cellElem = event.currentTarget.parentElement
+    const containerElem = cellElem?.parentElement
+
+    if (!(cellElem && containerElem)) {
+      return
     }
 
-    return 'calc(100% + var(--container-spacing))'
+    const columns = calcColumns(containerElem, cellElem)
+    setColumns(columns)
+    setHoverd({ id, index, row: Math.floor(index / columns) })
+
+    // hover的是当前行的第一个
+    if (index % columns === 0) {
+      setExtensionDirection('right')
+    }
+    // hover的是当前行的最后一个
+    if ((index + 1) % columns === 0) {
+      setExtensionDirection('left')
+    }
+  }
+
+  const handleMouseLeave = () => {
+    setHoverd(initialValueOfHoverd)
+  }
+
+  const calcLeftAndRight = (index: number) => {
+    // 未hover任何卡片
+    if (!hoverd.id) {
+      return { left: '0', right: '0' }
+    }
+
+    // 非当前行
+    const currentRow = Math.floor(index / columns)
+    if (currentRow !== hoverd.row) {
+      return { left: '0', right: '0' }
+    }
+
+    const indexMirror = extensionDirection === 'right' ? index : columns - index
+    const hoverdIndexMirror =
+      extensionDirection === 'right' ? hoverd.index : columns - hoverd.index
+
+    let result = { left: '0', right: '0' }
+
+    if (indexMirror === hoverdIndexMirror) {
+      result = { left: '0', right: 'calc(-100% - var(--container-spacing))' }
+    } else if (indexMirror > hoverdIndexMirror) {
+      result = {
+        left: 'calc(100% + var(--container-spacing))',
+        right: 'calc(-100% - var(--container-spacing))',
+      }
+    }
+
+    if (extensionDirection === 'right') {
+      return result
+    }
+
+    return { left: result.right, right: result.left }
   }
 
   return (
@@ -39,6 +117,7 @@ function Movies() {
       templateColumns="repeat(auto-fill, minmax(160px, 1fr))"
       gap="var(--container-spacing)"
       overflowX="hidden"
+      onMouseLeave={handleMouseLeave}
     >
       {posts.map((post, index) => (
         <Box key={index} pos="relative" className="group">
@@ -50,25 +129,24 @@ function Movies() {
             <Box></Box>
           </AspectRatio>
           <Card.Root
-            w="full"
             h="full"
             rounded="lg"
             overflow="hidden"
             pos="absolute"
             top="0"
-            left={calcLeft(String(index))}
-            transition="all 0.4s"
+            left={calcLeftAndRight(index).left}
+            right={calcLeftAndRight(index).right}
             zIndex="docked"
-            _groupHover={{
-              w: 'calc(200% + var(--container-spacing))',
-            }}
-            onMouseEnter={() => setHoverd(String(index))}
-            onMouseLeave={() => setHoverd('')}
+            transition="all 0.4s"
+            onMouseEnter={(event) =>
+              handleMouseEnter(event, String(index), index)
+            }
           >
             <Card.Body p="0">
-              <AspectRatio ratio={2 / 3}>
-                <Image src={post.src} alt={post.name}></Image>
-              </AspectRatio>
+              {/* <Image src={post.src} alt={post.name}></Image> */}
+              <Center bg="bg.muted" h="full">
+                {index}
+              </Center>
             </Card.Body>
           </Card.Root>
         </Box>
